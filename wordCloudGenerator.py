18fdf904a15b348c5027b7a0794b10b67351a4d5
BuddingIntents <79275023+buddingintents@users.noreply.github.com>
2025-01-30 03:56:45 +0530

    Update wordCloudGenerator.py

--- wordCloudGenerator.py
+++ wordCloudGenerator.py
@@ -289,7 +289,7 @@ if uploaded_file:
                 wc_image = wordcloud.to_image()
                 if not hashlib.sha256(secret_access.encode()).hexdigest() == SECRET_HASH:
                     wc_image = add_watermark(wc_image, "Generated @ Ankit's WordCloud")
-                else
+                else:
                     st.eeror(default_secret)
                 st.image(wc_image, use_container_width=True)
                 
